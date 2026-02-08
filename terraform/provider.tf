d992a22386abcae02936444a05259c600b111472
sonerg <sonerg@gmail.com>
2026-02-08 21:56:15 +0000

    keychange2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,18 @@
+# provider "aws" {
+#   region  = var.region
+#   profile = var.profile 
+#   alias = "terra"    # need for deploymnet provide block under resource
+# }
+
 provider "aws" {
-  region  = var.region
-  profile = var.profile 
-  alias = "terra"    # need for deploymnet provide block under resource
+  region = var.region
+}
+
+provider "aws" {
+  alias  = "terra"
+  region = var.region
+
+  assume_role {
+    role_arn = "arn:aws:iam::508261694247:role/Terra_user_role" # var.terra_role_arn
+  }
 }
