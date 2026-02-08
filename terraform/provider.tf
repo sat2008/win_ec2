99bb91ba3fd202c4e0d9732fdf990efb43111bc9
sonerg <sonerg@gmail.com>
2026-02-08 22:14:35 +0000

    keychange6

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,11 +8,11 @@ provider "aws" {
   region = var.region
 }
 
-provider "aws" {
-  alias  = "terra"
-  region = var.region
+# provider "aws" {
+#   alias  = "terra"
+#   region = var.region
 
-  assume_role {
-    role_arn = "arn:aws:iam::508261694247:role/Terra_user_role" # var.terra_role_arn
-  }
-}
+#   assume_role {
+#     role_arn = "arn:aws:iam::508261694247:role/Terra_user_role" # var.terra_role_arn
+#   }
+# }
