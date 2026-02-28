81f05b398418017913554a87b9c86f0786a2c01b
sonerg <sonerg@gmail.com>
2026-02-28 10:23:22 +0000

    backendchange2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
-    
+
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-repo-sg"
-    key            = "git/winec2/terraform.tfstate"
-    region         = "eu-west-2"
-    use_lockfile = "true"
-    encrypt        = true
+    bucket       = "terraform-state-repo-sg"
+    key          = "git/winec2/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+    encrypt      = true
   }
 }
