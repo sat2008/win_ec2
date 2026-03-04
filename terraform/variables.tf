f0bbe92b715ddcf44f8008f1de52e9198b203800
sonerg <sonerg@gmail.com>
2026-03-04 09:45:47 +0000

    sg_changes2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,8 +25,27 @@ variable "additional_disks" {
 }
 
 # Security Groups
-variable "ingress_rules" { type = list(any) }
-variable "egress_rules" { type = list(any) }
+variable "ingress_rules" {
+  type = list(object({
+    description      = optional(string)
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = optional(list(string))
+    ipv6_cidr_blocks = optional(list(string))
+  }))
+}
+
+variable "egress_rules" {
+  type = list(object({
+    description      = optional(string)
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = optional(list(string))
+    ipv6_cidr_blocks = optional(list(string))
+  }))
+}
 
 #variable "server_name" {}
 #variable "security_group_ids" {}
